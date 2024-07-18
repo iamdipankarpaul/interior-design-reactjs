93acb2174d937d9b147c7f48c7f9348fafcceb29
dipankarpaul2k <dipankarpaul2k@gmail.com>
2024-07-18 21:23:09 +0530

    update scroll to top btn

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,8 +1,10 @@
+import { useMediaQuery } from "@mantine/hooks";
 import { IconArrowNarrowUp } from "@tabler/icons-react";
 import { useState, useEffect } from "react";
 
 const ScrollToTop = () => {
   const [isVisible, setIsVisible] = useState(false);
+  const sm = useMediaQuery("(min-width: 640px)");
 
   const handleScroll = () => {
     const scrollTop = window.scrollY;
@@ -29,9 +31,9 @@ const ScrollToTop = () => {
       style={{
         display: isVisible ? "flex" : "none",
       }}
-      className="btn btn-circle cursor-pointer fixed right-5 bottom-5 mix-blend-difference max-md:btn-sm"
+      className="btn btn-circle cursor-pointer fixed right-5 bottom-5 mix-blend-difference max-sm:btn-sm"
     >
-      <IconArrowNarrowUp stroke={3} />
+      <IconArrowNarrowUp stroke={sm ? 3 : 2} />
     </button>
   );
 };
