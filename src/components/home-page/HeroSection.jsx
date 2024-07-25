ea5efecdee8af1804883a38dfa11f7c8df9ce960
Dipankar Paul <dipankarpaul2k@gmail.com>
2024-07-25 15:44:24 +0530

    minor styling changes

--- src/components/home-page/HeroSection.jsx
+++ src/components/home-page/HeroSection.jsx
@@ -58,7 +58,7 @@ const HeroSection = () => {
   return (
     <>
       <section
-        className={`px-4 sm:px-6 md:px-8 lg:px-12 pt-12 md:pt-16 pb-28 max-sm:pb-36`}
+        className={`px-4 sm:px-6 md:px-8 lg:px-12 pt-12 md:pt-16 pb-28 max-md:pb-10`}
       >
         {/* hero heading text */}
         <h1
@@ -91,7 +91,7 @@ const HeroSection = () => {
           </div>
 
           {/* floating contact card */}
-          <div className="w-fit p-4 md:p-8 bg-white border-t-4 border-t-accent absolute shadow-lg animate-bounce-slow top-[80%] sm:top-[10%] right-[1%] sm:right-[3%] z-10">
+          <div className="w-fit p-4 md:p-8 bg-white border-t-4 border-t-accent absolute shadow-lg animate-bounce-slow top-[80%] sm:top-[10%] right-[1%] sm:right-[3%] z-10 max-sm:hidden">
             <p className="text-sm sm:text-base md:text-lg mb-4 ">Contact:</p>
             <p className="text-sm sm:text-base font-bold">+91 96099 30991</p>
             <p className="text-sm sm:text-base font-bold">
@@ -100,7 +100,7 @@ const HeroSection = () => {
           </div>
 
           {/* floating clients card */}
-          <div className="w-fit p-4 md:p-8 bg-white border-t-4 border-t-accent absolute shadow-lg animate-bounce-slow top-[95%] sm:top-[65%] left-[1%] sm:left-[3%] z-10">
+          <div className="w-fit p-4 md:p-8 bg-white border-t-4 border-t-accent absolute shadow-lg animate-bounce-slow top-[95%] sm:top-[65%] left-[1%] sm:left-[3%] z-10 max-sm:hidden">
             <p className="text-xl md:text-3xl font-bold text-accent">104</p>
             <p className="text-sm sm:text-base">HAPPY CLIENTS</p>
             <div className="my-4 md:my-8"></div>
