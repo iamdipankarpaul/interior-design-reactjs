0ff55e0b38cba0125b0c53fcf63306b20cfc902c
dipankarpaul2k <dipankarpaul2k@gmail.com>
2024-07-25 11:58:42 +0530

    add faqs page

--- src/components/faq-page/FaqsList.jsx
+++ src/components/faq-page/FaqsList.jsx
@@ -1,8 +1,21 @@
-const FaqsList = () => {
+const FaqsList = ({ faqs }) => {
   return (
-    <>
-      <div>FaqsList</div>
-    </>
+    <div className="max-w-screen-lg mx-auto">
+      {faqs.map((faq) => (
+        <div
+          key={faq.id}
+          className="collapse collapse-plus bg-base-200 my-4 rounded-none border-l-4 border-l-accent"
+        >
+          <input type="radio" name="my-accordion-3" defaultChecked />
+          <div className="collapse-title text-xl font-medium">
+            {faq.question}
+          </div>
+          <div className="collapse-content">
+            <p>{faq.answer}</p>
+          </div>
+        </div>
+      ))}
+    </div>
   );
 };
 
--- src/pages/FaqsPage.jsx
+++ src/pages/FaqsPage.jsx
@@ -1,3 +1,4 @@
+import FaqsList from "../components/faq-page/FaqsList";
 import PageHeader from "../components/PageHeader";
 import { faqsPageContent } from "../data/faq-page-content";
 
@@ -8,6 +9,9 @@ const FaqsPage = () => {
         title={faqsPageContent.title}
         subtitle={faqsPageContent.subtitle}
       />
+      <section className="px-4 sm:px-6 lg:px-8 my-4 lg:mb-8">
+        <FaqsList faqs={faqsPageContent.faqs} />
+      </section>
     </>
   );
 };
